556d46caf4e38aea673919fa408487d284f09d87
Madhur <madhur.ahuja@akosha.in>
2015-09-14 15:18:46 +0530

    updated

--- js/app.js
+++ js/app.js
@@ -305,6 +305,17 @@ chatApp.directive('keyboardShortcut', function($http, $log, $templateCache, $com
                 });
 
             }
+
+
+            $(document).mouseup(function(e) {
+                var container = $("#popupholder");
+
+                if (!container.is(e.target) // if the target of the click isn't the container...
+                    && container.has(e.target).length === 0) // ... nor a descendant of the container
+                {
+                    destroyPopup();
+                }
+            });
         }
     }
 });
